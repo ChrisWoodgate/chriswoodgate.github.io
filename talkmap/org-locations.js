eab809567555db70c599236d74fc67305779a7b6
Chris Woodgate <c.woodgate@warwick.ac.uk>
2024-07-04 14:20:52 +0200

    Adding talk at ICM 2024

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,6 +5,16 @@ var addressPoints = [
     -1.5608359523684596
   ],
   [
+    "Crowne Plaza Manchester City Centre, Manchester, UK",
+    53.4786158,
+    -2.2348648408382052
+  ],
+  [
+    "Mercure Chester Abbotts Well, Chester, UK",
+    53.18538065,
+    -2.848130045723777
+  ],
+  [
     "Hynes Convention Center, Boston, USA",
     42.3479992,
     -71.087485
@@ -20,14 +30,19 @@ var addressPoints = [
     -1.2034364590042501
   ],
   [
+    "Bologna Congress Center, Bologna, Italy",
+    44.5058723,
+    11.343341648913043
+  ],
+  [
     "EPFL, Lausanne, Switzerland",
     46.518659400000004,
     6.566561505148001
   ],
   [
-    "Birmingham, UK",
-    52.4594522,
-    -1.9474166
+    "University of Birmingham, Birmingham, UK",
+    52.45046225,
+    -1.9319950061788347
   ],
   [
     "Northeastern University, Boston, USA",
@@ -43,15 +58,5 @@ var addressPoints = [
     "Universidad Complutense de Madrid, Madrid, Spain",
     40.4506776,
     -3.717818286212092
-  ],
-  [
-    "Crowne Plaza Manchester City Centre, Manchester, UK",
-    53.4786158,
-    -2.2348648408382052
-  ],
-  [
-    "Mercure Chester Abbotts Well, Chester, UK",
-    53.18538065,
-    -2.848130045723777
   ]
 ];
